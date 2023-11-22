cbe243070396a83d687daf7a4891fe83919548b2
IanEzeP <pereyra.ian00@gmail.com>
2023-11-22 19:09:24 -0300

    whatever

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,49 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { PeliculaAltaComponent } from './components/pelicula-alta/pelicula-alta.component';
+import { PeliculaListadoComponent } from './components/pelicula-listado/pelicula-listado.component';
+import { ActorListadoComponent } from './components/actor-listado/actor-listado.component';
+import { ActorAltaComponent } from './components/actor-alta/actor-alta.component';
+import { TablaPeliculaComponent } from './components/tabla-pelicula/tabla-pelicula.component';
+import { ErrorComponent } from './components/error/error.component';
+import { PaisesService } from './servicios/paises.service';
+import { FormsModule } from '@angular/forms';
+import { DetallePeliculaComponent } from './components/detalle-pelicula/detalle-pelicula.component';
+import { initializeApp,provideFirebaseApp, } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { TablaPaisesComponent } from './components/tabla-paises/tabla-paises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    BusquedaComponent,
+    PeliculaAltaComponent,
+    PeliculaListadoComponent,
+    ActorListadoComponent,
+    ActorAltaComponent,
+    TablaPeliculaComponent,
+    ErrorComponent,
+    DetallePeliculaComponent,
+    TablaPaisesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [ PaisesService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
